c1279075f0e1683855c217bbba26d0b233ae2af7
Yaroslav Gusev <yarigusev@gmail.com>
2024-12-25 13:47:29 +0300

    corrects

--- conftest.py
+++ conftest.py
@@ -3,61 +3,61 @@ from selenium import webdriver
 import pytest
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from dotenv import load_dotenv
 import os
 
 from resources import attach
 
+DEFAULT_BROWSER_VERSION = "126.0"
 
-# DEFAULT_BROWSER_VERSION = "126.0"
-#
-#
-# def pytest_addoption(parser):
-#     parser.addoption(
-#         '--browser_version',
-#         default=DEFAULT_BROWSER_VERSION,
-#     )
-#
-#
-# @pytest.fixture(scope="session", autouse=True)
-# def load_env():
-#     load_dotenv()
-#
-#
-# @pytest.fixture(scope='session', autouse=True)
-# def open_browser(request):
-#     browser_version = request.config.getoption('browser_version') or DEFAULT_BROWSER_VERSION
-#
-#     options = Options()
-#     selenoid_capabilities = {
-#         "browserName": "chrome",
-#         "browserVersion": browser_version,
-#         "selenoid:options": {
-#             "enableVNC": True,
-#             "enableVideo": True
-#         }
-#     }
-#     options.capabilities.update(selenoid_capabilities)
-#
-#     selenoid_login = os.getenv("SELENOID_LOGIN")
-#     selenoid_pass = os.getenv("SELENOID_PASS")
-#     selenoid_url = os.getenv("SELENOID_URL")
-#
-#     driver = webdriver.Remote(
-#         command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
-#         options=options
-#     )
-#
-#     browser.config.driver = driver
-#
-#     yield driver
-#
-#     attach.add_screenshot(browser)
-#     attach.add_logs(browser)
-#     attach.add_html(browser)
-#     attach.add_video(browser)
-#
-#     driver.quit()
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default=DEFAULT_BROWSER_VERSION,
+    )
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='session', autouse=True)
+def open_browser(request):
+    browser_version = request.config.getoption('browser_version') or DEFAULT_BROWSER_VERSION
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+
+    driver = webdriver.Remote(
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
+
+    yield driver
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+    driver.quit()
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -65,9 +65,19 @@ def open_sber_url():
     chrome_options = Options()
     chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.page_load_strategy = 'eager'
-    browser.config.driver = webdriver.Chrome(options=chrome_options)
 
-    browser.config.window_width = 1280
-    browser.config.window_height = 724
+    service = Service(ChromeDriverManager().install())
+    browser = webdriver.Chrome(service=service, options=chrome_options)
 
-    browser.config.base_url = 'https://rabota.sber.ru'
+    browser.set_window_size(1280, 724)
+    base_url = 'https://rabota.sber.ru'
+
+    browser.config = {
+        'window_width': 1280,
+        'window_height': 724,
+        'base_url': base_url,
+    }
+
+    yield browser
+
+    browser.quit()
