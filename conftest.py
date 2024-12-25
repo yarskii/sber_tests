d071c673d0d9661e4f18204efc9341bc7a5043e0
Yaroslav Gusev <yarigusev@gmail.com>
2024-12-25 15:58:53 +0300

    corrects

--- conftest.py
+++ conftest.py
@@ -1,12 +1,9 @@
 from selene import browser
 from selenium import webdriver
 import pytest
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 from dotenv import load_dotenv
 import os
-
 from resources import attach
 
 DEFAULT_BROWSER_VERSION = "126.0"
@@ -62,12 +59,10 @@ def open_browser(request):
 
 @pytest.fixture(scope='session')
 def open_sber_url(open_browser):
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'
-    driver_options.add_argument('--ignore-certificate-errors')
-
-    browser.config.driver_options = driver_options
-
+    chrome_options = Options()
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.page_load_strategy = 'eager'
+    browser.config.driver = webdriver.Chrome(options=chrome_options)
     browser.config.window_width = 1280
     browser.config.window_height = 724
     browser.config.base_url = 'https://rabota.sber.ru'
